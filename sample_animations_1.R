15ea2d1d953760d1a9a6df0c7e752b1db3498a90
Cole Jacobson <39979582+jacole3@users.noreply.github.com>
2024-01-03 13:20:16 -0800

    Update and rename animations_5.R to sample_animations_1.R

--- sample_animations_1.R
+++ sample_animations_1.R
@@ -3,30 +3,26 @@ library(tidyverse)
 library(gganimate)
 source("https://raw.githubusercontent.com/mlfurman3/gg_field/main/gg_field.R")
 
-#reading in all four files:
+# reading in all four files:
 tracking_w1 <- read_csv("tracking_week_1.csv")
 games<- read_csv("games.csv")
 players <- read_csv("players.csv")
 plays <- read_csv("plays.csv")
 
-
-##joining tracking data to the plays data:
-
+# Joining tracking data to the plays data:
 tracking_w1 <- tracking_w1 %>%
   left_join(plays, by = c("gameId", "playId")) %>%
   mutate(ball_carrier_indicator = ifelse(ballCarrierId==nflId,1,0)) %>%
   mutate(x = ifelse(playDirection == "left", 120-x, x),
          y = ifelse(playDirection == "left", 53.3 - y, y))
 
-#setting variables: (you can adjust these on your own)
+# Set variables for sample animation: (you can adjust these on your own)
 ballcarrier <- 'CeeDee Lamb'
 game_id <- 2022091113
 play_id <-155
 
-
 sample_ceedee_pass<- tracking_w1%>%
   filter(ballCarrierDisplayName==ballcarrier & gameId==game_id & playId==play_id)
-
 
 ##disregard this plot, use the next one:
 sample_ceedee_pass %>%
@@ -42,7 +38,6 @@ sample_ceedee_pass %>%
             color='black',
             size=5)
 
-
 #attributes used for plot. first is away, second is football, third is home.
 cols_fill <- c("dodgerblue1", "#663300", "firebrick1")
 cols_col <- c("#000000", "#663300", "#000000")
@@ -54,7 +49,6 @@ nFrames <- max(sample_ceedee_pass$frameId)
 #plotting
 anim <- ggplot() +
   
-  
   #creating field underlay
   gg_field(yardmin = 0, yardmax = 122) +
   
@@ -63,13 +57,11 @@ anim <- ggplot() +
                                         color = "forestgreen"),
         panel.grid = element_blank()) +
   
-  
   #setting size and color parameters
   scale_size_manual(values = size_vals, guide = FALSE) + 
   scale_shape_manual(values = shape_vals, guide = FALSE) +
   scale_fill_manual(values = cols_fill, guide = FALSE) + 
   scale_colour_manual(values = cols_col, guide = FALSE) +
-  
   
   #adding players
   geom_point(data = sample_ceedee_pass, aes(x = x,
@@ -87,7 +79,6 @@ anim <- ggplot() +
             colour = "white", 
             vjust = 0.36, size = 3.5) + 
   
-  
   #titling plot with play description
   labs(title = plot_title) +
   
@@ -95,6 +86,5 @@ anim <- ggplot() +
   transition_time(frameId)  +
   ease_aes("linear") + 
   NULL
-
 
 anim
